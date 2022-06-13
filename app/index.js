2a45cecf5f642191ebe8c72990005b71998e5600
User <hh360284@gmail.com>
2022-06-13 11:42:57 +0200

    Adding some comments

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
 const fs = require("fs/promises");
 const _ = require("lodash");
 
-async function getData() {
+/**
+ * Reading a json file and return content
+ *
+ * @param {String} path
+ * @returns {Object}
+ */
+async function readJsonFile(path) {
   try {
     const data = await fs.readFile("app/data.json", { encoding: "utf8" });
     return JSON.parse(data);
@@ -10,10 +16,22 @@ async function getData() {
   }
 }
 
+/**
+ * Concate the given array with another array
+ *
+ * @param {Array} arr
+ * @returns {Array}
+ */
 function concat(arr) {
   return _.concat(arr, [1, 2, 3]);
 }
 
+/**
+ * Geting sum of elements of the array
+ *
+ * @param {Array} arr
+ * @returns {Number}
+ */
 function sum(arr) {
   return arr.reduce((prev, cur) => {
     return prev + cur;
@@ -21,7 +39,7 @@ function sum(arr) {
 }
 
 module.exports = {
-  getData,
+  readJsonFile,
   concat,
   sum,
 };
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const app = require("./app");
+const { readJsonFile, sum, concat } = require("./app");
 
 async function result() {
-  const data = await app.getData();
-  console.log(app.sum(data));
-  console.log(app.sum(app.concat(data)));
+  const data = await readJsonFile("./app/data.json");
+  console.log(sum(data));
+  console.log(sum(concat(data)));
 }
 
 result();
